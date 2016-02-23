f12a1fff54db239d9d67b5258712b9092a1f4d8b
Clayton Rieck <cjrieck123@gmail.com>
2016-02-22 20:20:04 -0500

    request articles over https

--- public/js/services/ArticleService.js
+++ public/js/services/ArticleService.js
@@ -1,5 +1,5 @@
 app.factory('articleService', ['$http', function($http){
-	return $http.get('http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=fab404982b3b43017c79d543caef8aca:19:74490677')
+	return $http.get('https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=fab404982b3b43017c79d543caef8aca:19:74490677')
 			.success(function(articleData) {
 				return articleData;
 			})
